7a1aa52f0946d08b0ddbec0c3becbe76fefc6820
Stefan <stefan.spita@moneyhub.com>
2021-01-27 15:09:43 +0000

    Add service to be called

---
+++ service-to-call/src/index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const fileUpload = require("express-fileupload")
+const config = require("config")
+const people = require("./data")
+
+const app = express()
+
+app.use(bodyParser.json({limit: "10mb"}))
+
+app.get("/people", (req, res) => {
+  res.send(people)
+})
+
+app.get("/people/:id", (req, res) => {
+  const {id: requestedId} = req.params
+  const person = people.find(({id}) => id === requestedId)
+  res.send(person)
+})
+
+app.post(
+  "/people/export",
+  fileUpload(),
+  (req, res) => {
+    console.log("Thank you for the file", req.file)
+    res.send(204)
+  },
+)
+
+app.listen(config.port, (err) => {
+  if (err) {
+    console.error("Error occurred starting the server", err)
+    process.exit(1)
+  }
+  console.log(`Server running on port ${config.port}`)
+})
