d1f92240f8e983f723c095866a27a8beb95af6c1
mohammedAK1991 <mohmed.ak.17@gmail.com>
2022-08-18 16:37:11 +0200

    chore: install helmet for security and cors to enable all CORS requests

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,21 +2,33 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const config = require("config")
 const request = require("request")
+const helmet = require("helmet")
+const cors = require("cors")
+
 
 const app = express()
+
+// Use Helmet for security
+app.use(helmet())
+
+// Enable all CORS requests.
+app.use(cors())
 
 app.use(bodyParser.json({limit: "10mb"}))
 
 app.get("/investments/:id", (req, res) => {
   const {id} = req.params
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
-    if (e) {
-      console.error(e)
-      res.send(500)
-    } else {
-      res.send(investments)
-    }
-  })
+  request.get(
+    `${config.investmentsServiceUrl}/investments/${id}`,
+    (e, r, investments) => {
+      if (e) {
+        console.error(e)
+        res.send(500)
+      } else {
+        res.send(investments)
+      }
+    },
+  )
 })
 
 app.listen(config.port, (err) => {
@@ -24,5 +36,7 @@ app.listen(config.port, (err) => {
     console.error("Error occurred starting the server", err)
     process.exit(1)
   }
-  console.log(`Server running on port ${config.port}`)
+  console.log(`Server running on the port ${config.port}`)
 })
+
+module.exports = app
