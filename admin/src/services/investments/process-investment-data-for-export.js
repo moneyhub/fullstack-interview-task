d48997e8961439598a0e69c2bef471d788f13168
João Chapa <reischapa@gmail.com>
2022-08-20 17:49:06 +0100

    feat(admin-investments-export): now using ramda instead of native es6 methods

--- admin/src/services/investments/process-investment-data-for-export.js
+++ admin/src/services/investments/process-investment-data-for-export.js
@@ -1,20 +1,21 @@
 const {getHoldingNameMemoized} = require("../financial-data")
+const R = require("ramda")
 
 async function processInvestmentsDataForExport(investmentsData) {
-  return Promise.all(investmentsData.reduce((acc, elem) => {
-    const promises = elem.holdings.map(async h => ({
-      "First Name": elem.firstName,
-      "Last Name": elem.lastName,
-      "Date": elem.date,
-      "Holding": await getHoldingNameMemoized(h.id),
-      "Value": elem.investmentTotal * h.investmentPercentage,
-    }))
+  return Promise.all(R.reduce((acc, investmentsDataElem) => {
+    const promises = R.map(async holding => ({
+      "First Name": investmentsDataElem.firstName,
+      "Last Name": investmentsDataElem.lastName,
+      "Date": investmentsDataElem.date,
+      "Holding": await getHoldingNameMemoized(holding.id),
+      "Value": investmentsDataElem.investmentTotal * holding.investmentPercentage,
+    }), investmentsDataElem.holdings)
 
     // doing it like this removes the need to flatten afterwards
     acc.push(...promises)
 
     return acc
-  }, []))
+  }, [],  investmentsData))
 }
 
 module.exports.processInvestmentsDataForExport = processInvestmentsDataForExport
